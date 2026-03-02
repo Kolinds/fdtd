4b841c006c9559b95bed359776290e92aaf90436
Kolinds <santimurielfigueroa2004@gmail.com>
2026-03-02 21:01:12 +0100

    Understanding commits

--- bare_bones.py
+++ bare_bones.py
@@ -1,13 +1,6 @@
 import numpy as np
-import numba as nb
+from numba import njit
 import h5py
-
-SPACE_SIZE = 200
-TOTAL_TIME = 50000
-
-ez = np.zeros(SPACE_SIZE)
-hy = np.zeros(SPACE_SIZE)
-imp0 = 337.0
 
 
 def hpf5_maxValue(file_name, dataset_name):
@@ -31,35 +24,53 @@ def hpf5_normalization(file_name, dataset_name, maxValue):
             dset[actual_line:actual_line+DIM_JUMP, ...] = norm_matrix
 
 
+def update_magnetic_field(hy, ez, imp0, SPACE_SIZE):
+    for m in range(0, SPACE_SIZE - 1):
+        hy[m] = hy[m] + (1.0 / imp0) * (ez[m + 1] - ez[m])
+        
+    return hy
 
-@nb.njit
+
+def update_electric_field(ez, hy, imp0, SPACE_SIZE):
+    for m in range(1, SPACE_SIZE):
+        ez[m] = ez[m] + imp0 * (hy[m] - hy[m - 1])
+        
+    return ez
+
+
+
+SPACE_SIZE = 200
+TOTAL_TIME = 5000
+TIME_BUFFER = 100
+
+ez_matrix= np.zeros((TIME_BUFFER, SPACE_SIZE))
+hy_matrix = np.zeros((TIME_BUFFER, SPACE_SIZE))
+imp0 = 337.0
+
 with h5py.File("wave_data.hdf5", "w") as f:
-    hy_data = f.create_dataset("mag_fdata", (TOTAL_TIME, SPACE_SIZE))
-    ez_data = f.create_dataset("elec_fdata", (TOTAL_TIME, SPACE_SIZE))
+    hy_dset = f.create_dataset("mag_fdata", (TOTAL_TIME, SPACE_SIZE))
+    ez_dset = f.create_dataset("elec_fdata", (TOTAL_TIME, SPACE_SIZE))
 
     # Time loop
+    
     for qTime in range (0, TOTAL_TIME):
-        # Update all magnetic field values half a time step
-        for m in range (0, SPACE_SIZE - 2):
-            hy[m] = hy[m] + (1/imp0) * (ez[m + 1] - ez[m])
-
-        # Update all electric field values half a time step
-        for m in range (1, SPACE_SIZE - 1):
-            ez[m] = ez[m] + imp0 * (hy[m] - hy[m - 1])
+        qBuffer = qTime % TIME_BUFFE
+        hy_matrix[qBuffer, :] = update_magnetic_field(hy_matrix[qBuffer, :], ez_matrix[qBuffer, :], imp0, SPACE_SIZE)
+        ez_matrix[qBuffer, :] = update_electric_field(ez_matrix[qBuffer, :], hy_matrix[qBuffer, :], imp0, SPACE_SIZE)
         
-        
-        ez_data[qTime, 0:SPACE_SIZE] = ez[:]
-        hy_data[qTime, 0:SPACE_SIZE] = hy[:]
+        if (qBuffer == 0)  and (qTime != 0):
+            ez_dset[qTime - TIME_BUFFER:qTime, 0:SPACE_SIZE] = ez_matrix
+            hy_dset[qTime - TIME_BUFFER:qTime, 0:SPACE_SIZE] = hy_matrix
 
-        ez[0] = np.exp((-(qTime - 30)**2)/100.)
+        ez_matrix[qBuffer, 0] = np.exp((-(qBuffer - 30)**2)/100.)
 
-
+"""
 #Normalization of the values
 max_mag = hpf5_maxValue("wave_data.hdf5", "mag_fdata")
 hpf5_normalization("wave_data.hdf5", "mag_fdata", max_mag)
 max_elec = hpf5_maxValue("wave_data.hdf5", "elec_fdata")
 hpf5_normalization("wave_data.hdf5", "elec_fdata", max_elec)
+"""
 
-
-
+# holi uwu
 
