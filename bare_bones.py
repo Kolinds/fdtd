fff524d0a0a04aab5602ca8520e3ded3214c2dce
Kolinds <santimurielfigueroa2004@gmail.com>
2026-03-06 17:56:11 +0100

    Finished introduction

--- bare_bones.py
+++ bare_bones.py
@@ -10,8 +10,8 @@ def hpf5_maxValue(file_name, dataset_name):
         dset = f[dataset_name]
         for actual_line in range(0, dset.shape[0], DIM_JUMP):
             jump_matrix = dset[actual_line:actual_line+DIM_JUMP, ...]
-            actual_max = np.max(jump_matrix)
-            if (abs(total_max) < (actual_max)): total_max = actual_max
+            actual_max = np.max(np.abs(jump_matrix))
+            if (total_max < actual_max): total_max = actual_max
     return total_max
 
 
@@ -25,46 +25,81 @@ def hpf5_normalization(file_name, dataset_name, maxValue):
             dset[actual_line:actual_line+DIM_JUMP, ...] = norm_matrix
 
 @njit
-def update_magnetic_field(hy, ez, imp0, SPACE_SIZE):
+def update_magnetic_field(hy, ez, chyh, chye, SPACE_SIZE):
     for m in range(0, SPACE_SIZE - 1):
-        hy[m] = hy[m] + (1.0 / imp0) * (ez[m + 1] - ez[m])  
+        hy[m] = chyh[m] * hy[m] + chye[m] * (ez[m + 1] - ez[m])  
     return hy
 
 @njit
-def update_electric_field(ez, hy, imp0, SPACE_SIZE):
-    for m in range(1, SPACE_SIZE):
-        ez[m] = ez[m] + imp0 * (hy[m] - hy[m - 1])
+def update_electric_field(ez, hy, ceze, cezh, SPACE_SIZE):
+    for m in range(1, SPACE_SIZE - 1):
+        ez[m] = ceze[m] * ez[m] + cezh[m] * (hy[m] - hy[m - 1])
     return ez
 
 
 
-SPACE_SIZE = 200
+SPACE_SIZE = 240
 TOTAL_TIME = 1000
 TIME_BUFFER = 100
+IMP0 = 337.0
+LOSS_LAYER = 180
+LOSS = 0.02
 
+#Arrays for saving electric and E and H field values, and buffers
 ez_array= np.zeros(SPACE_SIZE)
-hy_array= np.zeros(SPACE_SIZE)
+hy_array= np.zeros(SPACE_SIZE - 1)
 ez_buffer= np.zeros((TIME_BUFFER, SPACE_SIZE))
-hy_buffer= np.zeros((TIME_BUFFER, SPACE_SIZE))
+hy_buffer= np.zeros((TIME_BUFFER, SPACE_SIZE - 1))
 
-imp0 = 337.0
+#Arrays for permitivity, permeability and loss of materials 
+ceze_array = np.ones(SPACE_SIZE)
+cezh_array = np.ones(SPACE_SIZE)
+chyh_array = np.ones(SPACE_SIZE - 1)
+chye_array = np.ones(SPACE_SIZE - 1)
+
+for m in range(0, SPACE_SIZE - 1):
+    if (m < LOSS_LAYER): #Free space
+        chyh_array[m] = 1.0
+        chye_array[m] = 1 / IMP0
+    else:
+        chyh_array[m] = (1.0 - LOSS) / (1.0 + LOSS)
+        chye_array[m] = (1.0 / IMP0) / (1.0 + LOSS)
+
+for m in range(0, SPACE_SIZE):
+    if (m < 100): #Free space
+        ceze_array[m] = 1.0
+        cezh_array[m] = IMP0
+    elif (m < LOSS_LAYER):   #Dielectric
+        ceze_array[m] = 1.0
+        cezh_array[m] = IMP0 / 9.0
+    else: #Lossy Boundary Layer
+        ceze_array[m] = (1.0 - LOSS) / (1.0 + LOSS)
+        cezh_array[m] = (IMP0 / 9.0) / (1.0 + LOSS)
+
+
 
 with h5py.File("wave_data.hdf5", "w") as f:
-    hy_dset = f.create_dataset("mag_fdata", (TOTAL_TIME, SPACE_SIZE))
+    hy_dset = f.create_dataset("mag_fdata", (TOTAL_TIME, SPACE_SIZE - 1))
     ez_dset = f.create_dataset("elec_fdata", (TOTAL_TIME, SPACE_SIZE))
 
     # Time loop
     for qTime in range (0, TOTAL_TIME):
 
-        #Absorbing boundary condition (ABC) mag
+        """
+        Absorbing boundary condition (ABC) mag, deprecated
         hy_array[SPACE_SIZE-1] = hy_array[SPACE_SIZE-2]
-        hy_array = update_magnetic_field(hy_array, ez_array, imp0, SPACE_SIZE)
+        """
+        hy_array = update_magnetic_field(hy_array, ez_array, chyh_array, chye_array, SPACE_SIZE - 1)
         #Correcion TFSF para quitar el efecto del incidente
-        hy_array[49] -= np.exp((-(qTime - 30)**2)/100.) / imp0
+        hy_array[49] -= np.exp((-(qTime - 30)**2)/100.) / IMP0
 
         #Absorbing boundary condition (ABC) elec
         ez_array[0] = ez_array[1]
-        ez_array = update_electric_field(ez_array, hy_array, imp0, SPACE_SIZE)
+        """
+        This ABC does not work inside dielectric
+        ez_array[SPACE_SIZE-1] = ez_array[SPACE_SIZE-2]
+        """
+        ez_array = update_electric_field(ez_array, hy_array, ceze_array, cezh_array, SPACE_SIZE)
         #Correcion TFSF para añadir el efecto del incidente
         ez_array[50] += np.exp((-(qTime + 0.5 - (-0.5) - 30)**2)/100.)
 
@@ -74,7 +109,7 @@ with h5py.File("wave_data.hdf5", "w") as f:
 
         if (buffer_index == 0)  and (qTime != 0):
             ez_dset[qTime - TIME_BUFFER:qTime, 0:SPACE_SIZE] = ez_buffer
-            hy_dset[qTime - TIME_BUFFER:qTime, 0:SPACE_SIZE] = hy_buffer
+            hy_dset[qTime - TIME_BUFFER:qTime, 0:SPACE_SIZE - 1] = hy_buffer
 
         ez_buffer[buffer_index, :] = ez_array
         hy_buffer[buffer_index, :] = hy_array
--- wave_animation.py
+++ wave_animation.py
@@ -3,24 +3,27 @@ import numpy as np
 import h5py
 import matplotlib.animation as animation
 
+
+
+H5FILE_NAME="wave_data.hdf5"
+DATASET_NAME = "elec_fdata"
+TOTAL_TIME = 1000
+SPACE_SIZE = 240
+
+#Visual
 plt.style.use('dark_background')
 fig, ax1 = plt.subplots(figsize=(5, 5), facecolor="#232230",
                  layout='constrained')
 
 ax1.spines['bottom'].set_position(('data', 0))
-ax1.set(xlim = (0, 200),ylim = (-1, 1))
+ax1.set(xlim = (0, SPACE_SIZE),ylim = (-1, 1))
 ax1.spines["bottom"].set_color("black")
 ax1.spines["bottom"].set_linewidth(2.0)
 for borde in ("left", "right"):
     ax1.spines[borde].set(color="black", linewidth=2.0)
 
 
-H5FILE_NAME="wave_data.hdf5"
-DATASET_NAME = "elec_fdata"
-TOTAL_TIME = 1000
-SPACE_SIZE = 200
-
-
+#Animacion
 space_array = np.linspace(0, SPACE_SIZE, SPACE_SIZE)
 wave_line, = ax1.plot(space_array, np.zeros_like(space_array), color = "red")
 time_text = ax1.text(0.05, 0.95, 'CUCARACHA', transform=ax1.transAxes, fontsize=12)
@@ -29,12 +32,12 @@ with h5py.File(H5FILE_NAME, "r") as f:
         all_data = f[DATASET_NAME][:TOTAL_TIME, :SPACE_SIZE]
 
 def anime_func(frame):
-    frame = frame
+    frame = frame * 1
     present_data = all_data[frame, :]
     wave_line.set_ydata(present_data)
     time_text.set_text(f'Frame: {frame}')
     return wave_line, time_text
 
-ani = animation.FuncAnimation(fig=fig, func=anime_func, frames=TOTAL_TIME, interval = 10)
+ani = animation.FuncAnimation(fig=fig, func=anime_func, frames=TOTAL_TIME//1, interval = 20)
 
 plt.show()
