2de29a8e7de37ed6aa7bcdbccaa709bde91f4900
Kolinds <santimurielfigueroa2004@gmail.com>
2026-03-09 18:18:54 +0100

    Config applied to wave_anim

--- bare_bones.py
+++ bare_bones.py
@@ -1,26 +1,26 @@
 import hdf5_handler as h5h
 import grid_struct as gr
 import maxwell_update as upd
-import config as cg
+import config as cf
 
 
-hdf5_file = h5h.HDF5Writer(cg.FILE_NAME, cg.TIME_BUFFER, cg.TOTAL_TIME, cg.SPACE_SIZE)
-grid = gr.Grid(cg.SPACE_SIZE)
+hdf5_file = h5h.HDF5Writer(cf.FILE_NAME, cf.TIME_BUFFER, cf.TOTAL_TIME, cf.SPACE_SIZE)
+grid = gr.Grid(cf.SPACE_SIZE)
 
-hdf5_file.open_file(cg.EDSET_NAME, cg.HDSET_NAME)
-grid.place_materials(cg.SPACE_SIZE, cg.LOSS, cg.LOSS_LAYER, cg.IMP0)
+hdf5_file.open_file(cf.EDSET_NAME, cf.HDSET_NAME)
+grid.place_materials(cf.SPACE_SIZE, cf.LOSS, cf.LOSS_LAYER, cf.IMP0)
 
-for qTime in range (0, cg.TOTAL_TIME):
+for qTime in range (0, cf.TOTAL_TIME):
 
-    grid.hy = upd.update_magnetic_field(grid.hy, grid.ez, grid.chyh, grid.chye, cg.SPACE_SIZE - 1)
-    grid.TFSF_boundary(qTime, 30, 0.5, -0.5, 0, 0, 100, 1, cg.IMP0)
+    grid.hy = upd.update_magnetic_field(grid.hy, grid.ez, grid.chyh, grid.chye, cf.SPACE_SIZE - 1)
+    grid.TFSF_boundary(qTime, 30, 0.5, -0.5, 0, 0, 100, 1, cf.IMP0)
     grid.abc_boundary()
-    grid.ez = upd.update_electric_field(grid.ez, grid.hy, grid.ceze, grid.cezh, cg.SPACE_SIZE)
+    grid.ez = upd.update_electric_field(grid.ez, grid.hy, grid.ceze, grid.cezh, cf.SPACE_SIZE)
     
     hdf5_file.update_file(qTime, grid.ez, grid.hy)
 
-h5h.normalization(cg.FILE_NAME, cg.HDSET_NAME, cg.BUFFER_JUMP, h5h.maxValue(cg.FILE_NAME, cg.HDSET_NAME, 100)) #Normalization of the H-field
-h5h.normalization(cg.FILE_NAME, cg.EDSET_NAME, cg.BUFFER_JUMP, h5h.maxValue(cg.FILE_NAME, cg.EDSET_NAME, 100)) #Normalization of the E-field
+h5h.normalization(cf.FILE_NAME, cf.HDSET_NAME, cf.BUFFER_JUMP, h5h.maxValue(cf.FILE_NAME, cf.HDSET_NAME, 100)) #Normalization of the H-field
+h5h.normalization(cf.FILE_NAME, cf.EDSET_NAME, cf.BUFFER_JUMP, h5h.maxValue(cf.FILE_NAME, cf.EDSET_NAME, 100)) #Normalization of the E-field
 
 hdf5_file.close_file()
 
--- wave_animation.py
+++ wave_animation.py
@@ -2,13 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 import matplotlib.animation as animation
+import config as cf
 
-
-
-H5FILE_NAME="wave_data.hdf5"
-DATASET_NAME = "elec_fdata"
-TOTAL_TIME = 1000
-SPACE_SIZE = 240
 
 #Visual
 plt.style.use('dark_background')
@@ -16,7 +11,7 @@ fig, ax1 = plt.subplots(figsize=(5, 5), facecolor="#232230",
                  layout='constrained')
 
 ax1.spines['bottom'].set_position(('data', 0))
-ax1.set(xlim = (0, SPACE_SIZE),ylim = (-1, 1))
+ax1.set(xlim = (0, cf.SPACE_SIZE),ylim = (-1, 1))
 ax1.spines["bottom"].set_color("black")
 ax1.spines["bottom"].set_linewidth(2.0)
 for borde in ("left", "right"):
@@ -24,12 +19,12 @@ for borde in ("left", "right"):
 
 
 #Animacion
-space_array = np.linspace(0, SPACE_SIZE, SPACE_SIZE)
+space_array = np.linspace(0, cf.SPACE_SIZE, cf.SPACE_SIZE)
 wave_line, = ax1.plot(space_array, np.zeros_like(space_array), color = "red")
 time_text = ax1.text(0.05, 0.95, 'CUCARACHA', transform=ax1.transAxes, fontsize=12)
 
-with h5py.File(H5FILE_NAME, "r") as f:
-        all_data = f[DATASET_NAME][:TOTAL_TIME, :SPACE_SIZE]
+with h5py.File(cf.FILE_NAME, "r") as f:
+        all_data = f[cf.EDSET_NAME][:cf.TOTAL_TIME, :cf.SPACE_SIZE]
 
 def anime_func(frame):
     frame = frame * 1
@@ -38,6 +33,6 @@ def anime_func(frame):
     time_text.set_text(f'Frame: {frame}')
     return wave_line, time_text
 
-ani = animation.FuncAnimation(fig=fig, func=anime_func, frames=TOTAL_TIME//1, interval = 20)
+ani = animation.FuncAnimation(fig=fig, func=anime_func, frames=cf.TOTAL_TIME//1, interval = 20)
 
 plt.show()
